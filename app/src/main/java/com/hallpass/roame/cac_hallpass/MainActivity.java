5028014175125040f3ab0c3cc165367c56dfe2b5
Roame Hildebrand <roamehildebrand@gmail.com>
2019-09-09 20:50:47 -0400

    Initial testing of basic app.

--- app/src/main/java/com/hallpass/roame/cac_hallpass/MainActivity.java
+++ app/src/main/java/com/hallpass/roame/cac_hallpass/MainActivity.java
@@ -2,12 +2,34 @@ package com.hallpass.roame.cac_hallpass;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textTest = findViewById(R.id.test_textView);
+
+        Button test = (Button) findViewById(R.id.test_button);
+        test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateText();
+            }
+        });
     }
+
+
+
+
+    public void updateText(){
+        textTest.setText("Not yee-haw hours.");
+    }
+
 }
