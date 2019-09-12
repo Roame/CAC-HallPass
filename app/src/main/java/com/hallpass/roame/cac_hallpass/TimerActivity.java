bf79712cb4e5d3ca7687cae790e830f2dfb36bf2
Roame Hildebrand <roamehildebrand@gmail.com>
2019-09-12 19:55:06 -0400

    Created several new activities to handle the pass functionality of the app. Also messed around with some layout behaviors.

--- app/src/main/java/com/hallpass/roame/cac_hallpass/TimerActivity.java
+++ app/src/main/java/com/hallpass/roame/cac_hallpass/TimerActivity.java
@@ -1,17 +1,13 @@
 package com.hallpass.roame.cac_hallpass;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class TimerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_timer);
     }
 }
