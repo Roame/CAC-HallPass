7b76035612d5ace0d43371dce72d6d4c3a239f65
Roame Hildebrand <roamehildebrand@gmail.com>
2019-09-13 15:55:25 -0400

    Experimented with some gui designs.

--- app/src/main/java/com/hallpass/roame/cac_hallpass/MainActivity.java
+++ app/src/main/java/com/hallpass/roame/cac_hallpass/MainActivity.java
@@ -7,11 +7,31 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.time.Instant;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Button passBtn;
+    private Intent createPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createPass = new Intent(this, PassAuthActivity.class);
+
+        passBtn = findViewById(R.id.PassCreateBtn);
+        passBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(createPass);
+            }
+        });
+
+
+
+
+
     }
 }
